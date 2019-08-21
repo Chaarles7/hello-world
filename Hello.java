0ffa8e619268dd42400bb2a0d5a4e5e5154ab6b4
Chaarles7 <54373726+Chaarles7@users.noreply.github.com>
2019-08-21 14:32:21 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+	public static void printHeading(){
+	System.out.println("Your Name");
+	System.out.println("Java Project Name");
+	System.out.println("Project Description");
+	System.out.println("version Date");
+	System.out.println("Course Number and Section");
+	System.out.println();
+	}
+
+	public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
